22db31121dd0f029d507a615f77adb326e4dc27c
lehuuduyen <lehuuduyen96@gmail.com>
2023-12-06 15:47:21 +0700

    update

--- header.php
+++ header.php
@@ -9,12 +9,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
     <meta name="csrf-param" content="_csrf">
-    <meta name="csrf-token"
-        content="VH-6aqb2D65Mozx1nfsUsqxhNEwQ-FTcxNCmwXergoA4KO4r8Jpm3C7FSj3OyEHawAwFJFOvOJapvcOyFPzYyQ==">
+    <meta name="csrf-token" content="VH-6aqb2D65Mozx1nfsUsqxhNEwQ-FTcxNCmwXergoA4KO4r8Jpm3C7FSj3OyEHawAwFJFOvOJapvcOyFPzYyQ==">
     <script async="" src="https://mc.yandex.ru/metrika/tag.js"></script>
     <script type="text/javascript">
-        (function (m, e, t, r, i, k, a) {
-            m[i] = m[i] || function () {
+        (function(m, e, t, r, i, k, a) {
+            m[i] = m[i] || function() {
                 (m[i].a = m[i].a || []).push(arguments)
             };
             m[i].l = 1 * new Date();
@@ -29,28 +28,30 @@
             accurateTrackBounce: true
         });
     </script>
-    <title><?=(!empty($title))?$title:"Pornlulu"?></title>
+    <title><?= (!empty($title)) ? $title : "Pornlulu" ?></title>
     <meta name="keywords" content="">
-    <meta name="description" content="<?=(!empty($video))?$video[3]:"最大的線上無碼情色A片網站，日本AV視頻。本站有百萬高清免費的日本AV視頻全部免費觀看。本站特點就是沒廣告，播放流暢，全部高清。"?>">
+    <meta name="description" content="<?= (!empty($video)) ? $video[3] : "最大的線上無碼情色A片網站，日本AV視頻。本站有百萬高清免費的日本AV視頻全部免費觀看。本站特點就是沒廣告，播放流暢，全部高清。" ?>">
     <meta property="og:type" content="article" />
-    <meta property="og:title" content="<?=(!empty($title))?$title:"Pornlulu"?>" />
-    <meta property="og:description" content="<?=(!empty($video))?$video[3]:"最大的線上無碼情色A片網站，日本AV視頻。本站有百萬高清免費的日本AV視頻全部免費觀看。本站特點就是沒廣告，播放流暢，全部高清。"?>" />
-    <meta property="og:image" content="<?=$imgMeta?>" />
+    <meta property="og:title" content="<?= (!empty($title)) ? $title : "Pornlulu" ?>" />
+    <meta property="og:description" content="<?= (!empty($video)) ? $video[3] : "最大的線上無碼情色A片網站，日本AV視頻。本站有百萬高清免費的日本AV視頻全部免費觀看。本站特點就是沒廣告，播放流暢，全部高清。" ?>" />
+    <meta property="og:image" content="<?= $imgMeta ?>" />
     <meta property="og:release_date" content="2023-11-15" />
-    <link rel="canonical" href="<?=$fullHttp?>">
-    
+    <link rel="canonical" href="<?= $fullHttp ?>">
+    <link href="/style/img/logo.png" rel="icon">
+    <link href="/style/img/logo.png" rel="apple-touch-icon">
+    <link href="/style/css/adminlte.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare2.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/style/css/site.css?9.4" rel="stylesheet">
+    <script src="https://www.googletagmanager.com/gtag/js?id=G-0GGDBPM7QH"></script>
 </head>
 
-<body data-root="/" class="sidebar-mini layout-fixed layout-navbar-fixed sidebar-closed sidebar-collapse"
-    style="height: auto;position: absolute;" cz-shortcut-listen="true">
+<body data-root="/" class="sidebar-mini layout-fixed layout-navbar-fixed sidebar-closed sidebar-collapse" style="height: auto;position: absolute;" cz-shortcut-listen="true">
     <div class="wrapper">
 
-        <div id="fullPageModal"
-            style="position: fixed; width: 100%; height: 100%; background: rgba(255,255,255, 0.93); top: 0; left: 0; display: none; z-index:10000001">
+        <div id="fullPageModal" style="position: fixed; width: 100%; height: 100%; background: rgba(255,255,255, 0.93); top: 0; left: 0; display: none; z-index:10000001">
             <div style="max-width: 320px; margin: auto; text-align: center;" class="py-3">
                 <button type="button" class="btn btn-primary  btn-lg mb-2" onclick="hideFullPage(null)">關閉廣告</button>
-                <img src="https://www.pornlulu.com/images/friend/0/550.webp?1699513531" alt=""
-                    style="max-width:100%;max-height:100%;" onclick="hideFullPage('http://ad.dlxdedm.com/xhz-5/#/')">
+                <img src="https://www.pornlulu.com/images/friend/0/550.webp?1699513531" alt="" style="max-width:100%;max-height:100%;" onclick="hideFullPage('http://ad.dlxdedm.com/xhz-5/#/')">
             </div>
         </div>
 
@@ -77,19 +78,16 @@
             <nav class=" navbar navbar-expand navbar-dark" style="background: #a00">
                 <!-- Left navbar links -->
                 <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link" data-widget="pushmenu" href="#"><i
-                                class="fa fa-bars"></i></a></li>
+                    <li class="nav-item"><a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a></li>
                 </ul>
                 <a class="navbar-brand py-0" href="/">
                     Pornlulu </a>
 
                 <div class="d-none d-md-block" style="height: 40px; overflow: hidden">
                     <ul id="w1" class="navbar-nav mr-auto links nav">
-                        <li class="nav-item"><a class="nav-link"
-                                href="https://go.bbrdbr.com/?userId=5a1df9435cc16ffadd407e46bd997ea39c05cfea3c40428a9975b085cafee25f"
-                                rel="nofollow" target="_blank">裸聊美女主播</a></li>
-                        <li class="nav-item"><a class="nav-link" href="https://www.hanime.xyz" rel="nofollow"
-                                target="_blank">H動漫網</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/list_blog" rel="nofollow" target="_blank">Blog</a></li>
+                         <li class="nav-item"><a class="nav-link" href="https://go.bbrdbr.com/?userId=5a1df9435cc16ffadd407e46bd997ea39c05cfea3c40428a9975b085cafee25f" rel="nofollow" target="_blank">裸聊美女主播</a></li>
+                        <li class="nav-item"><a class="nav-link" href="https://www.hanime.xyz" rel="nofollow" target="_blank">H動漫網</a></li>
                     </ul>
                 </div>
 
@@ -99,28 +97,25 @@
         <aside class="main-sidebar sidebar-light-primary elevation-4" style="z-index: 1000001;">
             <!-- Brand Logo -->
 
-            <a href="/" class="brand-link"><img class="brand-image img-circle elevation-3"
-                    src="/style/img/logo.png" alt=""> <span class="brand-text font-weight-light">Pornlulu</span> </a>
+            <a href="/" class="brand-link"><img class="brand-image img-circle elevation-3" src="/style/img/logo.png" alt=""> <span class="brand-text font-weight-light">Pornlulu</span> </a>
 
             <!-- Sidebar -->
             <div class="sidebar" style="overflow-y: auto;">
                 <!-- Sidebar Menu -->
                 <nav class="mt-2">
-                 
+
                     <ul id="w4" class="nav nav-pills nav-sidebar flex-column nav-compact nav-flat links">
                         <?php
-                          foreach($categories as $row) {
-                                ?>
-                        <li class="nav-item"><a class="nav-link"
-                                href="<?=str_replace('https://www.pornlulu.com','',$row['link'])?>"><i
-                                    class="nav-icon fa fa-circle"></i>
-                                <p><?=$row['name']?></p>
-                            </a>
-                        </li>
+                        foreach ($categories as $row) {
+                        ?>
+                            <li class="nav-item"><a class="nav-link" href="<?= str_replace('https://www.pornlulu.com', '', $row['link']) ?>"><i class="nav-icon fa fa-circle"></i>
+                                    <p><?= $row['name'] ?></p>
+                                </a>
+                            </li>
                         <?php
-                            }
-                        
-                        
+                        }
+
+
                         ?>
 
                     </ul>
@@ -136,6 +131,6 @@
                         <ol id="w5" class="mb-2 breadcrumb">
                             <li class="breadcrumb-item"><a href="/">首頁</a></li>
 
-                            <li class="breadcrumb-item"><a href="<?=$path?>"><?=$title?></a></li>
+                            <li class="breadcrumb-item"><a href="<?= $path ?>"><?= $title ?></a></li>
                         </ol>
                     </nav>
